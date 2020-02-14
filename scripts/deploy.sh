2ac48e45b46970da0181f4d64156e035e85845fd
thekarin <kissday--v@hanmail.net>
2020-02-14 13:06:08 +0900

    9장 travis ci - codeDeploy 연동

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot-service
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
